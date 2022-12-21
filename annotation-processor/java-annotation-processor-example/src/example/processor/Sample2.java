06486907514da6b439c5f61dce07b2a2b2770b27
_sCream <lieniece@gmail.com>
2022-12-21 18:43:11 +0900

    annotation-processor project used java

---
+++ annotation-processor/java-annotation-processor-example/src/example/processor/Sample2.java
@@ -0,0 +1,41 @@
+package example.processor;
+
+@ClassBuilder
+public class Sample2 {
+
+    private int id;
+
+    private String name;
+
+    @ClassBuilderIgnore
+    private Boolean enable;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+
+    @Override
+    public String toString() {
+        return "Sample [id=" + id + ", name=" + name + ", enable=" + enable + "]";
+    }
+}
